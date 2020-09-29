32cd5ad87405bd1e8aa62bf33c7b83c0735e45d9
rmanky <rmankaryous12@gmail.com>
2020-09-29 22:20:28 +0000

    gotta do hw

--- public/js/loader.js
+++ public/js/loader.js
@@ -1,1 +1,44 @@
-console.log("loaded");
+let folders = ["Asobo_A320_NEO_ABUSAN/", "Asobo_A320_NEO_AAL/"];
+
+// const fileStream = streamSaver.createWriteStream("download.zip");
+
+// fetch("/download", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify({ folders: folders }),
+// }).then((response) => {
+//   const readableStream = response.body;
+
+//   if (window.WritableStream && readableStream.pipeTo) {
+//     return readableStream
+//       .pipeTo(fileStream)
+//       .then(() => console.log("done writing"));
+//   }
+
+//   window.writer = fileStream.getWriter();
+
+//   const reader = res.body.getReader();
+//   const pump = () =>
+//     reader
+//       .read()
+//       .then((res) =>
+//         res.done ? writer.close() : writer.write(res.value).then(pump)
+//       );
+
+//   pump();
+// });
+
+// // abort so it dose not look stuck
+// window.onunload = () => {
+//   writableStream.abort()
+//   // also possible to call abort on the writer you got from `getWriter()`
+//   writer.abort()
+// }
+
+// window.onbeforeunload = evt => {
+//   if (!done) {
+//     evt.returnValue = `Are you sure you want to leave?`;
+//   }
+// }
--- server.js
+++ server.js
@@ -1,78 +1,105 @@
 // Load dotenv
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express'),
-    port = process.env.PORT || 3000;
-
-// Load the SDK for JavaScript
-var AWS = require('aws-sdk');
-// Set the Region 
-AWS.config.update({ region: 'us-east-1' });
+const fs = require("fs"),
+  join = require("path").join,
+  express = require("express"),
+  bodyParser = require("body-parser"),
+  XmlStream = require("xml-stream"),
+  AWS = require("aws-sdk"),
+  s3Zip = require("s3-zip"),
+  port = process.env.PORT || 3000;
 
 AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  region: "us-east-1",
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
-var s3 = new AWS.S3({ version: '2006-03-01' });
+var s3 = new AWS.S3({ version: "2006-03-01" });
 
-var localStorage = new Map();
+// var localStorage = new Map();
 
-let megapackBucket = {
-    Bucket: 'megapack'
-};
+// let megapackBucket = {
+//     Bucket: 'megapack'
+// };
 
-s3.listObjects(megapackBucket, (err, data) => {
-    data.Contents.forEach((value) => {
-        if (value.Key.includes("thumbnail.jpg")) {
-            console.log(value.Key.split("/")[0]);
-            console.log(value.Key);
-            var aircraftKey = value.Key.split("/")[0].split("_").splice(0, 2).join(" ");
-            var liveryKey = value.Key.split("/")[0];
-            var imageKey = value.Key;
-            var entry = {
-                liveryKey: liveryKey,
-                imageKey: imageKey
-            };
-            if (localStorage.has(aircraftKey)) {
-                localStorage.get(aircraftKey).push(entry);
-            } else {
-                let arr = new Array();
-                arr.push(entry);
-                localStorage.set(aircraftKey, arr);
-            }
-        }
-    });
+// s3.listObjects(megapackBucket, (err, data) => {
+//     data.Contents.forEach((value) => {
+//         if (value.Key.includes("thumbnail.jpg")) {
+//             console.log(value.Key.split("/")[0]);
+//             console.log(value.Key);
+//             var aircraftKey = value.Key.split("/")[0].split("_").splice(0, 2).join(" ");
+//             var liveryKey = value.Key.split("/")[0];
+//             var imageKey = value.Key;
+//             var entry = {
+//                 liveryKey: liveryKey,
+//                 imageKey: imageKey
+//             };
+//             if (localStorage.has(aircraftKey)) {
+//                 localStorage.get(aircraftKey).push(entry);
+//             } else {
+//                 let arr = new Array();
+//                 arr.push(entry);
+//                 localStorage.set(aircraftKey, arr);
+//             }
+//         }
+//     });
 
-    logStorage();
-});
+//     logStorage();
+// });
 
-const logStorage = () => {
-    localStorage.forEach((key) => {
-        key.forEach((entry) => {
-            console.log(entry.imageKey);
-        });
-    });
-}
+// const logStorage = () => {
+//     localStorage.forEach((key) => {
+//         key.forEach((entry) => {
+//             console.log(entry.imageKey);
+//         });
+//     });
+// }
 
 const app = express();
 
 app.use(express.static("public"));
+app.use(express.json());
 
 app.get("/", (_, response) => {
-    response.sendFile(__dirname + "/public/index.html");
+  response.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/download", (req, res) => {
+  const bucket = "megapack";
+  const region = "us-east-1";
+
+  const filesArray = [];
+
+  let folders = req.body.folders;
+
+  folders.forEach((folder, i) => {
+    const params = {
+        Bucket: bucket,
+        Prefix: folder
+    };
+    const files = s3.listObjects(params).createReadStream();
+    const xml = new XmlStream(files);
+    xml.collect("Key");
+    xml.on("endElement: Key", function (item) {
+      filesArray.push(folder + item["$text"].substr(folder.length));
+    });
+
+    if (i == folders.length - 1) {
+      xml.on("end", function () {
+        s3Zip
+          .archive(
+            { region: region, bucket: bucket, preserveFolderStructure: true },
+            "",
+            filesArray
+          )
+          .pipe(res);
+      });
+    }
+  });
 });
 
 const listener = app.listen(port, () => {
-    console.log("Your app is listening on port " + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
-
-// let testObject = {
-//     Bucket: 'megapack',
-//     Key: 'Asobo_b787_Virginatlantic/SimObjects/Airplanes/Asobo_b787_Virginatlantic/TEXTURE.ARG/thumbnail.jpg'
-// }
-
-// s3.getObject(testObject, (err, data) => {
-//     console.log(data);
-// });
-
